0d4cb2cdea57969f9c33faadd0c60a8ac6cf6caf
Rodrigo Luís Coelli <rodrigocoelli@hotmail.com>
2024-08-31 14:52:20 -0300

    Create dev.coelli.kt

---
+++ dev.coelli.kt
@@ -0,0 +1,21 @@
+data class Person(var name: String, var age: Int, var about: String) {
+    constructor() : this("", 0, "")
+}
+
+fun main() {
+
+    val coelli = Person()                                     // 1
+    val stringDescription = coelli.apply {                    // 2
+        name = "Rodrigo"                                       // 3
+        age = 40
+        about = "Android developer"
+    }.toString()                                            // 4
+
+    println(stringDescription)
+}
+
+data class Person(var name: String, var age: Int, var about: String) {
+    constructor() : this("", 0, "")
+}
+
+Open: https://pl.kotl.in/cHN1tLbEv?readOnly=true
